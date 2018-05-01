95987d2ae5f2042a736950c4f3e945e0c94fa50b
Niklas Strand <nikstra@users.noreply.github.com>
2018-05-01 19:40:10 +0200

    Add dummy tests to avoid warnings.

--- nikstra.CoreShopping.Core.Tests/Class1.cs
+++ nikstra.CoreShopping.Core.Tests/Class1.cs
@@ -1,8 +1,15 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
 
 namespace nikstra.CoreShopping.Core.Tests
 {
+    [TestFixture]
     public class Class1
     {
+        [Test]
+        public void Dummy()
+        {
+            Assert.True(true);
+        }
     }
 }
--- nikstra.CoreShopping.Service.Tests/Class1.cs
+++ nikstra.CoreShopping.Service.Tests/Class1.cs
@@ -1,8 +1,15 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
 
 namespace nikstra.CoreShopping.Service.Tests
 {
+    [TestFixture]
     public class Class1
     {
+        [Test]
+        public void Dummy()
+        {
+            Assert.True(true);
+        }
     }
 }
