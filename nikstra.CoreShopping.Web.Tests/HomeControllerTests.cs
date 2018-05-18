c3b3ef4ccef34defe64a0dd5ed2d21f5d3ac246c
Niklas Strand <nikstra@users.noreply.github.com>
2018-05-18 22:56:21 +0200

    Add unit tests for HomeController.

---
+++ nikstra.CoreShopping.Web.Tests/HomeControllerTests.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using nikstra.CoreShopping.Web.Controllers;
+using NUnit.Framework;
+using System;
+
+namespace nikstra.CoreShopping.Web.Tests
+{
+    [TestFixture]
+    public class HomeControllerTests
+    {
+        [Test]
+        public void GetIndex_ReturnsViewResult_WhenCalled()
+        {
+            var controller = new HomeController();
+
+            IActionResult result = controller.Index();
+
+            Assert.That(result, Is.InstanceOf<ViewResult>());
+        }
+
+        [Test]
+        public void GetAbout_ReturnsViewResult_WhenCalled()
+        {
+            var controller = new HomeController();
+
+            IActionResult result = controller.Index();
+
+            Assert.That(result, Is.InstanceOf<ViewResult>());
+        }
+
+        [Test]
+        public void GetContact_ReturnsViewResult_WhenCalled()
+        {
+            var controller = new HomeController();
+
+            IActionResult result = controller.Index();
+
+            Assert.That(result, Is.InstanceOf<ViewResult>());
+        }
+
+        [Test]
+        public void GetError_ReturnsViewResult_WhenCalled()
+        {
+            var controller = new HomeController();
+
+            IActionResult result = controller.Index();
+
+            Assert.That(result, Is.InstanceOf<ViewResult>());
+        }
+    }
+}
