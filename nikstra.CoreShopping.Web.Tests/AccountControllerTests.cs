d70d2b6fee40587d7875e560f35bb6257e246c0d
Niklas Strand <nikstra@users.noreply.github.com>
2018-05-28 02:18:22 +0200

    Add unit tests for AccountController.LoginWith2fa().

--- nikstra.CoreShopping.Web.Tests/AccountControllerTests.cs
+++ nikstra.CoreShopping.Web.Tests/AccountControllerTests.cs
@@ -260,6 +260,253 @@ namespace nikstra.CoreShopping.Web.Tests
         }
         #endregion
 
+        #region LoginWith2fa tests
+        [Test]
+        public void Get_LoginWith2fa_ShouldHaveHttpGetAttribute()
+        {
+            // Arrange
+            var type = typeof(AccountController);
+            var method = type.GetMethod(nameof(AccountController.LoginWith2fa), new[] { typeof(bool), typeof(string) });
+            var attributes = method.GetCustomAttributes(false);
+            var wantedAttributeType = typeof(HttpGetAttribute);
+
+            // Act
+            var result = attributes.FirstOrDefault(a => a.GetType() == wantedAttributeType);
+
+            // Assert
+            Assert.That(result, Is.Not.Null, $"No {wantedAttributeType.Name} found.");
+        }
+
+        [Test]
+        public void Get_LoginWith2fa_ShouldHaveAllowAnonymousAttribute()
+        {
+            // Arrange
+            var type = typeof(AccountController);
+            var method = type.GetMethod(nameof(AccountController.LoginWith2fa), new[] { typeof(bool), typeof(string) });
+            var attributes = method.GetCustomAttributes(false);
+            var wantedAttributeType = typeof(AllowAnonymousAttribute);
+
+            // Act
+            var result = attributes.FirstOrDefault(a => a.GetType() == wantedAttributeType);
+
+            // Assert
+            Assert.That(result, Is.Not.Null, $"No {wantedAttributeType.Name} found.");
+        }
+
+        [Test]
+        public async Task Get_LoginWith2fa_ReturnsViewAndModel_WhenUserWithTwoFactorAuthExists()
+        {
+            // Arrange
+            var userManager = CreateUserManagerMock();
+            var signInManager = CreateSignInManagerMock(userManager);
+            signInManager.GetTwoFactorAuthenticationUserAsync()
+                .Returns(Task.FromResult(CreateApplicationUser()));
+
+            var controller = CreateControllerInstance(signInManager);
+
+            // Act
+            var result = await controller.LoginWith2fa(false, "url");
+
+            // Assert
+            Assert.That(result, Is.InstanceOf<ViewResult>());
+            Assert.That((result as ViewResult).ViewData["ReturnUrl"], Is.EqualTo("url"));
+            Assert.That((result as ViewResult).Model, Is.InstanceOf<LoginWith2faViewModel>());
+            Assert.That(((result as ViewResult).Model as LoginWith2faViewModel).RememberMe, Is.False);
+        }
+
+        [Test]
+        public void Get_LoginWith2fa_ThrowsApplicationException_WhenUserWithTwoFactorAuthCanNotBeLoaded()
+        {
+            // Arrange
+            var userManager = CreateUserManagerMock();
+            var signInManager = CreateSignInManagerMock(userManager);
+            signInManager.GetTwoFactorAuthenticationUserAsync()
+                .Returns(Task.FromResult((ApplicationUser)null));
+
+            var controller = CreateControllerInstance(signInManager);
+
+            // Act
+            async Task Act()
+            {
+                var result = await controller.LoginWith2fa(false, "url");
+            }
+
+            // Assert
+            var ex = Assert.ThrowsAsync<ApplicationException>(Act);
+            Assert.That(ex.Message, Does.StartWith("Unable to load two-factor authentication user."));
+        }
+
+        [Test]
+        public void Post_LoginWith2fa_ShouldHaveHttpPostAttribute()
+        {
+            // Arrange
+            var type = typeof(AccountController);
+            var method = type.GetMethod(nameof(AccountController.LoginWith2fa),
+                new[] { typeof(LoginWith2faViewModel), typeof(bool), typeof(string) });
+            var attributes = method.GetCustomAttributes(false);
+            var wantedAttributeType = typeof(HttpPostAttribute);
+
+            // Act
+            var result = attributes.FirstOrDefault(a => a.GetType() == wantedAttributeType);
+
+            // Assert
+            Assert.That(result, Is.Not.Null, $"No {wantedAttributeType.Name} found.");
+        }
+
+        [Test]
+        public void Post_LoginWith2fa_ShouldHaveAllowAnonymousAttribute()
+        {
+            // Arrange
+            var type = typeof(AccountController);
+            var method = type.GetMethod(nameof(AccountController.LoginWith2fa),
+                new[] { typeof(LoginWith2faViewModel), typeof(bool), typeof(string) });
+            var attributes = method.GetCustomAttributes(false);
+            var wantedAttributeType = typeof(AllowAnonymousAttribute);
+
+            // Act
+            var result = attributes.FirstOrDefault(a => a.GetType() == wantedAttributeType);
+
+            // Assert
+            Assert.That(result, Is.Not.Null, $"No {wantedAttributeType.Name} found.");
+        }
+
+        [Test]
+        public async Task Post_LoginWith2fa_RedirectsToReturnUrl_WhenTwoFactorLoginIsSuccessful()
+        {
+            // Arrange
+            var userManager = CreateUserManagerMock();
+            var signInManager = CreateSignInManagerMock(userManager);
+            signInManager.GetTwoFactorAuthenticationUserAsync()
+                .Returns(Task.FromResult(CreateApplicationUser()));
+            signInManager.TwoFactorAuthenticatorSignInAsync(Arg.Any<string>(), Arg.Any<bool>(), Arg.Any<bool>())
+                .Returns(Task.FromResult(Microsoft.AspNetCore.Identity.SignInResult.Success));
+
+            var controller = CreateControllerInstance(signInManager);
+            controller.Url = Substitute.For<IUrlHelper>();
+            controller.Url.IsLocalUrl(Arg.Any<string>()).Returns(true);
+
+            var model = new LoginWith2faViewModel
+            {
+                RememberMachine = false,
+                RememberMe = false,
+                TwoFactorCode = "code"
+            };
+
+            // Act
+            var result = await controller.LoginWith2fa(model, false, "url");
+
+            // Assert
+            Assert.That(result, Is.InstanceOf<RedirectResult>());
+            Assert.That((result as RedirectResult).Url, Is.EqualTo("url"));
+        }
+
+        [Test]
+        public async Task Post_LoginWith2fa_ReturnsViewAndModel_WhenModelStateIsNotValid()
+        {
+            // Arrange
+            var userManager = CreateUserManagerMock();
+            var signInManager = CreateSignInManagerMock(userManager);
+
+            var controller = CreateControllerInstance(signInManager);
+            controller.ModelState.AddModelError("", "");
+
+            var model = new LoginWith2faViewModel
+            {
+                RememberMachine = false,
+                RememberMe = false,
+                TwoFactorCode = "code"
+            };
+
+            // Act
+            var result = await controller.LoginWith2fa(model, false, "url");
+
+            // Assert
+            Assert.That(result, Is.InstanceOf<ViewResult>());
+            Assert.That((result as ViewResult).Model, Is.InstanceOf<LoginWith2faViewModel>());
+            Assert.That((result as ViewResult).Model, Is.EqualTo(model));
+        }
+
+        [Test]
+        public async Task Post_LoginWith2fa_RedirectsToLockout_WhenAccountIsLockedOut()
+        {
+            // Arrange
+            var userManager = CreateUserManagerMock();
+            var signInManager = CreateSignInManagerMock(userManager);
+            signInManager.GetTwoFactorAuthenticationUserAsync()
+                .Returns(Task.FromResult(CreateApplicationUser()));
+            signInManager.TwoFactorAuthenticatorSignInAsync(Arg.Any<string>(), Arg.Any<bool>(), Arg.Any<bool>())
+                .Returns(Task.FromResult(Microsoft.AspNetCore.Identity.SignInResult.LockedOut));
+
+            var controller = CreateControllerInstance(signInManager);
+
+            var model = new LoginWith2faViewModel
+            {
+                RememberMachine = false,
+                RememberMe = false,
+                TwoFactorCode = "code"
+            };
+
+            // Act
+            var result = await controller.LoginWith2fa(model, false, "url");
+
+            // Assert
+            Assert.That(result, Is.InstanceOf<RedirectToActionResult>());
+            Assert.That((result as RedirectToActionResult).ActionName, Is.EqualTo(nameof(AccountController.Lockout)));
+        }
+
+        [Test]
+        public async Task Post_LoginWith2fa_AddModelErrorAndReturnsView_WhenAuthenticatorCodeIsInvalid()
+        {
+            // Arrange
+            var userManager = CreateUserManagerMock();
+            var signInManager = CreateSignInManagerMock(userManager);
+            signInManager.GetTwoFactorAuthenticationUserAsync()
+                .Returns(Task.FromResult(CreateApplicationUser()));
+            signInManager.TwoFactorAuthenticatorSignInAsync(Arg.Any<string>(), Arg.Any<bool>(), Arg.Any<bool>())
+                .Returns(Task.FromResult(Microsoft.AspNetCore.Identity.SignInResult.Failed));
+
+            var controller = CreateControllerInstance(signInManager);
+
+            var model = new LoginWith2faViewModel
+            {
+                RememberMachine = false,
+                RememberMe = false,
+                TwoFactorCode = "code"
+            };
+
+            // Act
+            var result = await controller.LoginWith2fa(model, false, "url");
+
+            // Assert
+            Assert.That(result, Is.InstanceOf<ViewResult>());
+            Assert.That((result as ViewResult).ViewData.ModelState.Count, Is.GreaterThan(0));
+        }
+
+        [Test]
+        public void Post_LoginWith2fa_ThrowsApplicationException_WhenUserWithTwoFactorAuthCanNotBeLoaded()
+        {
+            // Arrange
+            var userManager = CreateUserManagerMock();
+            var signInManager = CreateSignInManagerMock(userManager);
+            signInManager.GetTwoFactorAuthenticationUserAsync()
+                .Returns(Task.FromResult((ApplicationUser)null));
+
+            var controller = CreateControllerInstance(signInManager);
+
+            var model = new LoginWith2faViewModel();
+
+            // Act
+            async Task Act()
+            {
+                var result = await controller.LoginWith2fa(model, false, "url");
+            }
+
+            // Assert
+            var ex = Assert.ThrowsAsync<ApplicationException>(Act);
+            Assert.That(ex.Message, Does.StartWith("Unable to load user with ID"));
+        }
+        #endregion
+
         private ApplicationUser CreateApplicationUser() =>
             new ApplicationUser
             {
